930109425f35476a036dce3e71b086765eea0d4b
sayed <sayed.ahmed@nextory.com>
2023-08-06 12:17:56 +0530

    Initial setup

--- src/main/java/com/producer/demo/controller/Producer.java
+++ src/main/java/com/producer/demo/controller/Producer.java
@@ -3,6 +3,7 @@ package com.producer.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,9 +16,9 @@ public class Producer {
     @Autowired
     Publisher publisher;
 
-    @PostMapping("/publish")
-    public ResponseEntity.BodyBuilder sendMessage(@RequestParam("topic") final String topic, @RequestParam("message") final String message){
-        publisher.sendMessage(topic,message);
+    @GetMapping("/publish")
+    public ResponseEntity.BodyBuilder sendMessage(){
+        publisher.sendMessage("users","Hello World");
         return ResponseEntity.ok();
     }
 
