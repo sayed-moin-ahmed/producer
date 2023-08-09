601ea9b59756cdb8288c1365292a54a3f3d0ad9e
sayed <sayed.ahmed@nextory.com>
2023-08-09 21:05:25 +0530

    port changes

--- src/main/java/com/producer/demo/controller/Producer.java
+++ src/main/java/com/producer/demo/controller/Producer.java
@@ -4,9 +4,7 @@ package com.producer.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,9 +15,9 @@ public class Producer {
     Publisher publisher;
 
     @GetMapping("/publish")
-    public ResponseEntity.BodyBuilder sendMessage(){
+    public ResponseEntity<String> sendMessage(){
         publisher.sendMessage("users","Hello World");
-        return ResponseEntity.ok();
+        return ResponseEntity.ok("Success");
     }
 
 
